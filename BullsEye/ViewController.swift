936b9bc961115e7306c5716cb0a972a84c02946d
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-09 12:19:46 -0800

    ising of usual and outlet variables

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     var currentValue = 50
+    @IBOutlet weak var slider: UISlider!//Interface Builder doesn’t see any of your other variables, only the ones marked with @IBOutlet. You can refer to the slider object from anywhere inside the view controller using the slider variable.
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // The viewDidLoad() message is sent by UIKit as soon as the view controller loads its user interface from the storyboard file. At this point, the view controller isn’t visible yet, so this is a good place to set instance variables to their proper initial values.
+        
     }
 
     override func didReceiveMemoryWarning() {
