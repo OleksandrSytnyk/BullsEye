c8c950fee6ad8cbe4e97b1339db26ceea82f72a4
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-09 13:12:06 -0800

    Random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var targetValue: Int = 0
     var currentValue = 50
     @IBOutlet weak var slider: UISlider!//Interface Builder doesn’t see any of your other variables, only the ones marked with @IBOutlet. You can refer to the slider object from anywhere inside the view controller using the slider variable.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // The viewDidLoad() message is sent by UIKit as soon as the view controller loads its user interface from the storyboard file. At this point, the view controller isn’t visible yet, so this is a good place to set instance variables to their proper initial values.
-        
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,16 +25,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+        let message = "The value of the slider is: \(currentValue)"         + "\nThe target value is: \(targetValue)"
+        //The \n character sequence inserts a special “new line” character at that point, which will break up the text into two lines so the message is a little easier to read.
         let alert = UIAlertController(title: "Hello, World",
             message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 
 }
