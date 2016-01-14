ebc44e9709b14b5bfd0471a8efdd1554e991612e
OleksandrSytnyk <amsitnik@gmail.com>
2016-01-13 19:18:28 -0800

    Adding labels and creating of a title that depends

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var targetValue: Int = 0
+    var targetValue = 0
     var currentValue = 50
+    var score = 0
+    var round: Int = 0
     @IBOutlet weak var slider: UISlider!//Interface Builder doesn’t see any of your other variables, only the ones marked with @IBOutlet. You can refer to the slider object from anywhere inside the view controller using the slider variable.
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreaLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +33,20 @@ class ViewController: UIViewController {
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
         let points = 100 - difference
+        score += points
+        
+        var title: String //переменная определена именно здесь, а не в начале метода, потому что только здесь и используется
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
         let message = "You scored \(points) points"
-        let alert = UIAlertController(title: "Hello, World",
+        let alert = UIAlertController(title: title,
             message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         alert.addAction(action)
@@ -44,13 +60,16 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
     }
     
     func updateLabels() {
-targetLabel.text = String(targetValue)
+        targetLabel.text = String(targetValue)
+        scoreaLabel.text = String(score)
+        roundLabel.text = String(round)
     }
     
     
